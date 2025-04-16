b7e559f52f7f3534474acf8f580b822b1b1f5bf7
Satwikha <sblsatwikha@gmail.com>
2025-04-16 09:16:20 -0400

    implemntatn of feign client so that order service can access the product service APIs

--- OrderService/src/main/java/com/example/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/example/OrderService/service/OrderServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.OrderService.service;
 
 import com.example.OrderService.entity.Order;
+import com.example.OrderService.external.clients.ProductService;
 import com.example.OrderService.model.OrderRequest;
 import com.example.OrderService.repository.OrderRepository;
+import com.netflix.discovery.converters.Auto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,9 +17,14 @@ public class OrderServiceImpl implements OrderService{
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductService productService;
+
     @Override
     public long placeOrder(OrderRequest orderRequest) {
 
+        productService.reduceQuantity(orderRequest.getProductId(), orderRequest.getQuantity());
         Order order = Order.builder()
                 .productId(orderRequest.getProductId())
                 .orderStatus("CREATED")
